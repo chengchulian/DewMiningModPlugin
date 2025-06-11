0837f5f157f5251dbd3c8f8110abbdf589d3f39e
chengchulian <chengchulian@qq.com>
2025-06-11 16:28:56 +0800

    修正金块血量为当前关卡数+5,优化前期游戏体验

--- DewMiningModPlugin/patch/PropEntityPatch.cs
+++ DewMiningModPlugin/patch/PropEntityPatch.cs
@@ -18,9 +18,10 @@ public class PropEntityPatch
 
         if (__instance is not PropEnt_Stone_Gold gold) return;
 
+        var _zoneManager = NetworkedManagerBase<ZoneManager>.instance;
 
-        gold.Status.maxHealth = 10f;
-        gold.Status.Network_currentHealth = 10f;
+        var health = _zoneManager.currentZoneIndex + 5;
+        gold.Status.maxHealth = health;
+        gold.Status.Network_currentHealth = health;
     }
-
 }
