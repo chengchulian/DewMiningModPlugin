df6d0977c66b6dbe3e97abec755e660eda837abc
chengchulian <chengchulian@qq.com>
2025-06-12 17:31:38 +0800

    改为自定义金币掉落逻辑

--- DewMiningModPlugin/handler/StoneGoldOnDeathHandler.cs
+++ DewMiningModPlugin/handler/StoneGoldOnDeathHandler.cs
@@ -8,9 +8,29 @@ namespace DewMiningModPlugin.handler;
 public static class StoneGoldOnDeathHandler
 {
     private static readonly WaitForSeconds rewardDelayWait = new(0.8f);
+    
+    [Tooltip("区域缩放前应用的基础金币乘数。")] private static float baseGoldMultiplier = 1.5f;
+
+    [Tooltip("每个区域索引的额外金币乘数。")] private static float perZoneGoldMultiplier = 0.6f;
+
 
     public static void ProcessDeathRewards(PropEnt_Stone_Gold gold, EventInfoKill info)
     {
+        
+        var _zoneManager = NetworkedManagerBase<ZoneManager>.instance;
+        var _pickupManager = NetworkedManagerBase<PickupManager>.instance;
+        // 立即掉落金币
+        float goldValue = (baseGoldMultiplier + perZoneGoldMultiplier * _zoneManager.currentZoneIndex)
+                          * DewPlayer.humanPlayers.Count;
+
+        _pickupManager.DropGold(
+            isKillGold: false,
+            isGivenByOtherPlayer: false,
+            DewMath.RandomRoundToInt(goldValue),
+            gold.transform.position
+        );
+
+        
         var room = SingletonDewNetworkBehaviour<Room>.instance;
 
         var executor = MiningRewardManager.SelectReward();
--- DewMiningModPlugin/patch/PropEnt_Stone_GoldPatch.cs
+++ DewMiningModPlugin/patch/PropEnt_Stone_GoldPatch.cs
@@ -8,16 +8,18 @@ namespace DewMiningModPlugin.patch;
 [HarmonyPatch(typeof(PropEnt_Stone_Gold))]
 public class PropEntStoneGoldPatch
 {
-    [HarmonyPostfix]
+    [HarmonyPrefix]
     [HarmonyPatch("OnDeath")]
-    public static void OnDeathPostfix(PropEnt_Stone_Gold __instance, EventInfoKill info)
+    public static bool OnDeathPrefix(PropEnt_Stone_Gold __instance, EventInfoKill info)
     {
         // 2. 如果非服务器，不继续处理
         if (!__instance.isServer)
-            return ;
+            return false;
 
         // 3. 自定义掉落逻辑（不使用默认协程）
         StoneGoldOnDeathHandler.ProcessDeathRewards(__instance, info);
-        
+
+        // 4. 阻止原方法执行（跳过金币掉落）
+        return false;
     }
 }
